dbee92a6a8dc93d7ef537cf656698590a604ac17
coderconvoy <thebeaky@psmail.net>
2016-11-26 12:27:13 +0000

    templater now tracks a style cookie

--- temserver.go
+++ temserver.go
@@ -6,21 +6,43 @@ import (
 	"github.com/coderconvoy/templater/tempower"
 	"net/http"
 	"strings"
+	"time"
 )
 
 type Loose struct {
 	FileName string
+	Style    string
 }
 
 var templates *tempower.PowerTemplate
 
 func bigHandler(w http.ResponseWriter, r *http.Request) {
+	//Handle restyling options with a style cookie
+	styleC, cerr := r.Cookie("style")
+	style := ""
+	if cerr == nil {
+		style = styleC.Value
+	}
+
+	s2 := r.URL.Query().Get("style")
+	if s2 != "" {
+		style = s2
+		styleC = &http.Cookie{Name: "style", Value: style, Expires: time.Now().Add(time.Hour * 24)}
+	}
+
+	if styleC != nil {
+		http.SetCookie(w, styleC)
+	}
+
+	//allow errors
+
 	errs := make([]error, 0)
 	var err error
 	p := strings.TrimPrefix(r.URL.Path, "/")
 
 	fmt.Println("Path---", p)
 	// Empty for index
+
 	if p == "" {
 		err = templates.ExecuteTemplate(w, "index", nil)
 		if err != nil {
@@ -36,7 +58,7 @@ func bigHandler(w http.ResponseWriter, r *http.Request) {
 	//try template
 	for k, v := range p {
 		if v == '/' {
-			err = templates.ExecuteTemplate(w, p[:k], Loose{p[k+1:]})
+			err = templates.ExecuteTemplate(w, p[:k], Loose{p[k+1:], style})
 
 			if err == nil {
 				return
@@ -44,7 +66,7 @@ func bigHandler(w http.ResponseWriter, r *http.Request) {
 			errs = append(errs, err)
 		}
 	}
-	err = templates.ExecuteTemplate(w, p, Loose{""})
+	err = templates.ExecuteTemplate(w, p, Loose{"", style})
 	if err == nil {
 		return
 	}
@@ -52,7 +74,7 @@ func bigHandler(w http.ResponseWriter, r *http.Request) {
 
 	//Default
 	fmt.Println(errs)
-	err = templates.ExecuteTemplate(w, "loose", Loose{p + ".md"})
+	err = templates.ExecuteTemplate(w, "loose", Loose{p + ".md", style})
 	if err != nil {
 		fmt.Println(err)
 		fmt.Fprintln(w, err)
