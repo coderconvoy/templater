7f600fd5b80e4dcec31cdb78209a15c552dedbf8
coderconvoy <thebeaky@psmail.net>
2017-11-03 13:20:55 +0000

    Proxy now changes req.Host

--- cfm/proxy.go
+++ cfm/proxy.go
@@ -1,6 +1,8 @@
 package cfm
 
 import (
+	"fmt"
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 
@@ -24,6 +26,32 @@ func NewProxySite(lz lazyf.LZ, root string) (*proxySite, error) {
 		return nil, err
 	}
 	return &proxySite{DomList: lz.PStringAr("host", "Host"),
-		ReverseProxy: httputil.NewSingleHostReverseProxy(url),
+		//httputil.NewSingleHostReverseProxy(url),
+		ReverseProxy: &httputil.ReverseProxy{Director: proxyDirector(url)},
 	}, nil
+}
+
+func proxyDirector(target *url.URL) func(req *http.Request) {
+	return func(req *http.Request) {
+
+		fmt.Println("Redirecting:", req.URL)
+
+		targetQuery := target.RawQuery
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+		//req.URL.Path = singleJoiningSlash(target.Path, req.URL.Path)
+		req.Host = target.Host
+
+		if targetQuery == "" || req.URL.RawQuery == "" {
+			req.URL.RawQuery = targetQuery + req.URL.RawQuery
+		} else {
+			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+		}
+		if _, ok := req.Header["User-Agent"]; !ok {
+			// explicitly disable User-Agent so it's not set to default value
+			req.Header.Set("User-Agent", "")
+		}
+
+	}
+
 }
